5d9bb4df3bcf5228029c9d083a6d14ab2b89a165
Alexander Shikhalev <kiskamiska2000@gmail.com>
2019-03-23 13:03:26 +0700

    headers update

--- src/board.h
+++ src/board.h
@@ -1,6 +1,6 @@
 #ifndef BOARD_H_
 #define BOARD_H_
-
+#include <stdbool.h>
 void board(char a[][9]);
 bool move(char a[][9],char str[]);
 #endif
--- src/board_print.h
+++ src/board_print.h
@@ -1,5 +1,6 @@
 #ifndef BOARD_PRINT_H_
 #define BOARD_PRINT_H_
+#include <stdbool.h>
 void print(char a [][9]);
 
 #endif
