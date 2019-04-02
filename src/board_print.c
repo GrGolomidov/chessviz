f5b1349bb4e1aa52e3d64f58dc17f5014989692a
Alexander Shikhalev <kiskamiska2000@gmail.com>
2019-04-02 21:00:55 +0300

    Source code update by clang format

--- src/board_print.c
+++ src/board_print.c
@@ -2,11 +2,10 @@
 
 void output(char (*A)[10])
 {
-	int i, j;
-	for (i = 0; i <= 9; i++)
-	{
-		printf("\n\n");
-		for (j = 0; j <= 9; j++)
-		printf(" %c ",A[i][j]);
-	}
+    int i, j;
+    for (i = 0; i <= 9; i++) {
+        printf("\n\n");
+        for (j = 0; j <= 9; j++)
+            printf(" %c ", A[i][j]);
+    }
 }
